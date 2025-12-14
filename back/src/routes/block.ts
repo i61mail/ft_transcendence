87adc04268e4515b9e239fa10f3cd2ab202e492a
Ismail Rkik <isrkik@e2r4p12.1337.ma>
2025-12-14 13:50:14 +0100

    review block feature code

--- back/src/routes/block.ts
+++ back/src/routes/block.ts
@@ -7,17 +7,10 @@ import {
 } from '../controllers/block.controller';
 
 const blockRoutes = async (server: FastifyInstance) => {
-  // Block a user
-  server.post('/blocks/:userId', blockUser);
-
-  // Unblock a user
-  server.delete('/blocks/:userId', unblockUser);
-
-  // Get all blocked users for a user
-  server.get('/blocks/:userId', getBlockedUsers);
-
-  // Check if user is blocked
-  server.get('/blocks/:userId/check/:targetUserId', checkBlocked);
+  server.post('/blocks/:userId', blockUser); // Block a user
+  server.delete('/blocks/:userId', unblockUser); // Unblock a user
+  server.get('/blocks/:userId', getBlockedUsers); // Get all blocked users for a user
+  server.get('/blocks/:userId/check/:targetUserId', checkBlocked); // Check if user is blocked
 };
 
 export default blockRoutes;
